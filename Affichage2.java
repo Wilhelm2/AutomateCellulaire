2b7aea213aa7273afa2ec7b2b0f20f9ec97a3807
d.wilhelm <daniel.wilhelm@etu.unistra.fr>
2018-02-10 23:14:42 +0100

    correction bug + graphics + history + detect end

---
+++ Affichage2.java
@@ -0,0 +1,162 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JLabel ;
+import javax.swing.ImageIcon ;
+import java.io.IOException ;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.util.ArrayList ;
+import java.awt.Dimension ;
+import java.awt.Toolkit;
+import java.util.concurrent.TimeUnit;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JToolBar ;
+import javax.swing.JButton ;
+import java.awt.BorderLayout;
+import java.lang.Thread ;
+import java.awt.event.*;
+
+
+
+public class Affichage2 extends JPanel
+{
+    int column , row;
+    Automate Aut;
+    static final Object Obj = new Object(); // évite de faire des actions pendant que la prochaine étape est calculée
+    static Affichage2 A;
+    Color CelluleVivante = Color.BLUE;
+    Color CelluleMorte = Color.WHITE;
+    
+    public static void main(String[] args)
+        throws IOException, InterruptedException 
+    {
+        final JFrame frame= new JFrame();
+        
+        final Automate Aut = new Automate ( Integer.parseInt(args[0]), Integer.parseInt(args[1]), 10);
+        Aut.DefaultSetting();
+        Historique History = new Historique();
+        boolean changing = true;
+        
+        JToolBar toolbar = new JToolBar();
+        
+        JButton button = new JButton(new ImageIcon("arriere.png"));
+        button.addActionListener(new ActionListener(){
+            public void actionPerformed( ActionEvent ae )
+            {
+                System.out.println("va en arrière");
+                synchronized(Obj)
+                {
+                    if( History.index == 0 )
+                        return;
+                    else
+                    {
+                        Aut.tab = History.arriere();
+                        frame.repaint();
+                    }
+                }
+            }
+        });
+        toolbar.add(button);
+        
+        button = new JButton(new ImageIcon("stop.png"));
+        button.addActionListener(new ActionListener(){
+            public void actionPerformed( ActionEvent ae )
+            {
+                Aut.changeState();
+            }
+        });
+        toolbar.add(button);
+        
+        button = new JButton(new ImageIcon("avant.png"));
+        button.addActionListener(new ActionListener(){
+            public void actionPerformed( ActionEvent ae )
+            {
+                synchronized(Obj)
+                {
+                    if( History.index == History.maxindex ) 
+                        return ;
+                    else
+                    {
+                        Aut.tab = History.avant();
+                        frame.repaint();
+                    }
+                }
+            }
+        });
+        toolbar.add(button);
+        
+        frame.add(toolbar, BorderLayout.NORTH);
+
+        A = new Affichage2(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Aut);
+        frame.add(A);
+        frame.pack();
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
+        frame.setVisible(true);
+        History.addEvnt(Aut.tab);
+
+        while(changing)
+        {
+            try { TimeUnit.SECONDS.sleep(1); } 
+                catch (InterruptedException ex)  { Thread.currentThread().interrupt(); }
+            
+            while ( ! Aut.run ) 
+                Thread.sleep(100);
+            // Créé un nouveau JPanel à chaque fois pour éviter que le graphique soit directement 
+            // changé alors qu'il est affiché (sinon modifie l'affichage pour chaque changement de case)
+            
+            synchronized(Obj)
+            {
+                Aut.miseAJour();
+                frame.remove(A);
+                A = new Affichage2(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Aut);
+                frame.add(A);
+                frame.pack();
+                dim = Toolkit.getDefaultToolkit().getScreenSize();
+                frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
+                frame.setVisible(true);
+                History.addEvnt(Aut.tab);
+                if ( History.detectEnd())
+                {
+                    System.out.println("FIN");
+                    changing = false;
+                }
+            }
+        }
+    }
+    
+    public void paintComponent( Graphics g)
+    {
+        int i,j;
+        super.paintComponent(g);
+        for ( i= 1 ; i < row ;i++)
+            g.drawLine(0,20*i, column*20, 20*i);
+        for ( i= 1 ; i < column ;i++)
+            g.drawLine(20*i,0, 20*i, row*20);
+        
+        for(i=0 ; i < row ; i++)
+        {
+            for(j=0 ; j< column ; j++)
+            {
+                if ( Aut.tab.get(i).get(j).intValue()==1 )
+                    g.setColor(CelluleVivante);
+                else
+                    g.setColor(CelluleMorte);
+                g.fillOval(j*20,i*20,20,20);
+            }
+        }        
+    }
+    
+    Affichage2( int row,int column, Automate Aut)
+    {
+        this.column = column;
+        this.row = row;
+        this.setPreferredSize( new Dimension(column * 20, row*20));
+        this.Aut = Aut;
+    }
+    
+    
+}
---
+++ Automate.java
@@ -0,0 +1,125 @@
+
+import java.util.ArrayList ;
+import java.util.Random ;
+
+
+public class Automate
+{
+    ArrayList< ArrayList < Integer > > tab = new ArrayList < ArrayList<Integer> > ();
+    int column,row;
+    public boolean run = true;
+            
+            
+    public Automate (  int row , int column,int beginCells)
+    {
+        int tmp, tmp2;
+        int i,j,k;
+        this.column = column;
+        this.row = row;
+        
+        for(i=0 ; i < row ; i++)
+        {
+            tab.add( new ArrayList<Integer>() ) ;
+            for(j=0 ; j< column ; j++)
+                tab.get(i).add( new Integer(0) );
+        }
+        
+        //~ Random generator = new Random(System.currentTimeMillis());
+        //~ for (i = 0 ; i < beginCells ; i++)
+        //~ {
+            //~ tmp = (int)( generator.nextDouble()*( row ) ) ;
+            //~ tmp2 = (int)( generator.nextDouble()*( column ) ) ;
+            //~ for( j = Math.max( tmp -1 , 0) ; j < Math.min(tmp+1, column) ; j++)
+            //~ {
+                //~ for(k = Math.max(tmp2-1,0) ; k < Math.min(tmp2 +1, row) ; k++)
+                //~ {
+                    //~ System.out.println(" j = "+ j + " et k = " + k);
+                    //~ if( ! ( j== tmp && k == tmp2) )
+                        //~ tab.get(k).set(j, new Integer(1) );
+                    //~ else 
+                        //~ return;
+                //~ }
+            //~ }
+        //~ }
+    }
+    
+    public void changeState()
+    {
+        run = !run;
+    }
+    
+    public void miseAJour()
+    {
+        int i,j;
+        // commence par faire une copie du tableau
+        ArrayList< ArrayList<Integer> > Ltmp = new  ArrayList< ArrayList <Integer>> ();
+        for( i = 0 ; i< row;i++)
+        {
+            Ltmp.add( new ArrayList<Integer>() ) ;
+            for(j=0; j < column;j++)
+                Ltmp.get(i).add( new Integer(tab.get(i).get(j).intValue())) ; 
+        }
+        
+        for( i = 0 ; i< row;i++)
+        {
+            for(j=0; j < column;j++)
+            {
+                if( JeuVieSimple(i,j,Ltmp) )
+                {
+                    if( Ltmp.get(i).get(j).intValue() == 0) // cellule morte -> changement
+                        tab.get(i).set(j, new Integer(1));
+                }
+                else
+                {
+                    if( Ltmp.get(i).get(j).intValue() == 1) // cellule morte -> changement
+                        tab.get(i).set(j, new Integer(0));
+                }
+            }
+        }
+    }
+    
+
+    // Si 3 voisins vivants ->naît 
+    // Si vivante et entourée de 2 ou 3 cellules reste vivante
+    // Sinon meurt
+    public boolean JeuVieSimple( int row, int column, ArrayList<ArrayList<Integer>> Ltmp)
+    {
+        int i,j;
+        int VoisinsVivants = 0;
+        boolean Vivant = (Ltmp.get(row).get(column).intValue() == 1);
+        
+        for(i = ((row==0) ? 0: -1) ; i <= ((row == this.row-1) ? 0:1) ; i++)
+        {
+            for(j = ((column==0) ? 0: -1) ; j <= ((column == this.column-1) ? 0:1);j++)            
+            {
+                if ( i==0 && j== 0 ) // centre du "carré"
+                    continue ;
+                if( Ltmp.get(row+i).get(column+j).intValue() == 1)
+                    VoisinsVivants ++;
+            }
+        }
+        
+        if( VoisinsVivants == 3 && !Vivant )
+            return true;
+        else if( Vivant && ( VoisinsVivants == 2 || VoisinsVivants == 3) )
+            return true;
+        else
+            return false;
+    }
+    
+    public void DefaultSetting()
+    {
+        int i,j;
+        for(i=0;i<row; i++)
+        {
+            for(j=0;j<column;j++)
+            {
+                if( (i+j)%2 == 0)
+                    tab.get(i).set(j,new Integer(1));
+            }
+        }
+        
+    }
+    
+    
+}
